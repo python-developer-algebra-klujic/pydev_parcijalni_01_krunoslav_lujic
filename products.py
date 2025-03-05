4fdbab3e7c8bea4e261ac48e3b1251681a2586ab
klujic <krunoslav.lujic@predavaci.algebra.hr>
2025-03-05 20:47:38 +0100

    Summary

---
+++ products.py
@@ -0,0 +1,10 @@
+
+
+class Product:
+    def __init__(self,
+                 name: str,
+                 price : float,
+                 description=''):
+        self.name = name
+        self.price = price
+        self.description = description
